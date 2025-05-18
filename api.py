ef8a9038896ceb67bae6f700c77c0d8739b8d60f
huynhchinhan <nhan00554@gmail.com>
2025-05-18 20:58:13 +0700

    add data, change logic

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
+import pyarrow.parquet as pq
 import polars as pl
 from typing import Dict, List, Any
 from pydantic import BaseModel
 import json
+import os
+
+
+main_path = os.getcwd()
 
 app = FastAPI(title="Education Analytics API")
 
@@ -37,7 +42,7 @@ class Field(BaseModel):
 # API để lấy danh sách học sinh
 @app.get("/api/students", response_model=List[Student])
 async def get_students():
-    user_df = pl.read_parquet("/Users/cicilian/Desktop/CS313/merged_data/user_df.parquet")
+    user_df = pl.read_parquet(os.path.join(main_path, "data/user_df.parquet"))
     students = (
         user_df.rename({"user_id": "id", "user_name": "name"})
           .to_dicts()
@@ -52,7 +57,7 @@ async def get_students():
 # API để lấy danh sách môn học
 @app.get("/api/courses", response_model=List[Course])
 async def get_courses():
-    course_df = pl.read_parquet("/Users/cicilian/Desktop/CS313/merged_data/course_df.parquet")
+    course_df = pl.read_parquet(os.path.join(main_path, "data/course_df.parquet"))
     courses = (
         course_df.rename({"course_id": "id", "course_name": "name"})
             .to_dicts()
@@ -67,7 +72,7 @@ async def get_courses():
 # API để lấy danh sách field
 @app.get("/api/fields", response_model=List[Field])
 async def get_courses():
-    fields_df = pl.read_parquet("/Users/cicilian/Desktop/CS313/merged_data/field_df.parquet")
+    fields_df = pl.read_parquet(os.path.join(main_path, "data/field_df.parquet"))
     fields = (
         fields_df.rename({"field_name": "name"})
             .to_dicts()
@@ -82,7 +87,7 @@ async def get_courses():
 # API để lấy danh sách giáo viên
 @app.get("/api/teachers", response_model=List[Teacher])
 async def get_courses():
-    teacher_df = pl.read_parquet("/Users/cicilian/Desktop/CS313/merged_data/teacher_df.parquet")
+    teacher_df = pl.read_parquet(os.path.join(main_path, "data/teacher_df.parquet"))
     teacher = (
         teacher_df.rename({"teacher_id": "id"})
             .to_dicts()
